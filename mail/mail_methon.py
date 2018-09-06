c0e78c7275098e01c261873872ef371418200b8d
CORP\rzhang <1515547772@qq.com>
2018-09-06 10:32:59 +0800

    Initial commit

---
+++ mail/mail_methon.py
@@ -0,0 +1,11 @@
+import yagmail
+
+#链接邮箱服务器
+yag = yagmail.SMTP( user="15651930369@163.com", password="7572896zr", host='smtp.163.com')
+
+# 邮箱正文
+contents = ['This is the body, and here is just text http://somedomain/image.png',
+            'You can find an audio file attached.', '/local/path/song.mp3']
+
+# 发送邮件
+yag.send('1515547772@qq.com', '邮件标题', contents,["D://记事本.txt"])
